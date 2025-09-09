45a521715df9ea09b062f93ca434132761ac1b94
your-ko <>
2025-09-09 22:00:18 +0200

    Add some documentation

--- pkg/local/main.go
+++ pkg/local/main.go
@@ -1,3 +1,7 @@
+// Package 'local' implements local links validation
+// Local links are the links found in the given repository, which point to files in the same repository.
+// Example: [README](../../README.md)
+
 package local
 
 import (
@@ -25,7 +29,9 @@ func (proc *LinkProcessor) Process(ctx context.Context, url string, logger *zap.
 	if len(parts) != 1 && len(parts[0]) != 2 {
 		return fmt.Errorf("incorrect md syntax: %s", url)
 	}
-	_, err := os.ReadFile(fmt.Sprintf("%s/%s", proc.path, parts[0][1]))
+	fileName := fmt.Sprintf("%s/%s", proc.path, parts[0][1])
+	logger.Debug("validating local url", zap.String("filename", fileName))
+	_, err := os.ReadFile(fileName)
 	if err != nil {
 		return err
 	}
