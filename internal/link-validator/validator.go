1f72071ef5bb6a455dd52eab387fd7cf5e98c13b
your-ko <>
2025-09-19 22:31:55 +0200

    add more parameters

--- internal/link-validator/validator.go
+++ internal/link-validator/validator.go
@@ -35,9 +35,12 @@ type LinkValidador struct {
 }
 
 type Config struct {
-	BaseUrl string
-	Path    string
-	PAT     string
+	BaseUrl     string
+	Path        string
+	PAT         string
+	FileMasks   []string
+	ExcludePath string
+	LookupPath  string
 }
 
 func New(config Config) LinkValidador {
@@ -97,11 +100,11 @@ func (v *LinkValidador) ProcessFiles(ctx context.Context, filesList []string, lo
 	return stats
 }
 
-func (v *LinkValidador) GetFiles(root string, masks []string) ([]string, error) {
+func (v *LinkValidador) GetFiles(config Config) ([]string, error) {
 	var matchedFiles []string
 
 	matchesAnyMask := func(name string) bool {
-		for _, mask := range masks {
+		for _, mask := range config.FileMasks {
 			matched, err := filepath.Match(mask, name)
 			if err == nil && matched {
 				return true
@@ -110,7 +113,7 @@ func (v *LinkValidador) GetFiles(root string, masks []string) ([]string, error) 
 		return false
 	}
 
-	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+	err := filepath.WalkDir(config.LookupPath, func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
 			// Just skip files/dirs we can't read
 			return nil
