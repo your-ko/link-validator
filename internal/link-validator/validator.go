c22583e74eec7a4f4079611c03bc9428f8587aa5
your-ko <>
2025-09-29 06:25:15 +0200

    Don't even init

--- internal/link-validator/validator.go
+++ internal/link-validator/validator.go
@@ -42,7 +42,9 @@ type Config struct {
 
 func New(config Config) LinkValidador {
 	processors := make([]LinkProcessor, 0)
-	processors = append(processors, intern.New(config.BaseUrl, config.PAT))
+	if config.BaseUrl != "" {
+		processors = append(processors, intern.New(config.BaseUrl, config.PAT))
+	}
 	processors = append(processors, local.New())
 	processors = append(processors, external.New(config.BaseUrl))
 	return LinkValidador{processors}
