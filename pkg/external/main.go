d23a25503d2171cb01601fc49f49e306ed1c836f
your-ko <>
2025-10-19 12:06:34 +0200

    process status codes

--- pkg/external/main.go
+++ pkg/external/main.go
@@ -61,34 +61,49 @@ func (proc *LinkProcessor) Process(ctx context.Context, url string, _ string) er
 	req.Header.Set("Accept", "text/html")
 	req.Header.Set("User-Agent", "link-validator/1.0 (+https://github.com/your-ko/link-validator)")
 
-	r, err := proc.httpClient.Do(req)
+	resp, err := proc.httpClient.Do(req)
 	if err != nil {
 		return err
 	}
-	defer r.Body.Close()
-	if r.StatusCode < 200 || r.StatusCode >= 300 {
-		proc.logger.Debug("", zap.Int("statusCode", r.StatusCode))
+	defer resp.Body.Close()
+	switch {
+	case resp.StatusCode == 401 || resp.StatusCode == 403:
+		// we can proceed without authentication, so we don't know whether the url is alive.
+		// maybe in the future this will be improved
+		proc.logger.Info("requires auth", zap.Int("statusCode", resp.StatusCode), zap.String("url", url))
+		return nil
+	case resp.StatusCode == 404 || resp.StatusCode == 410:
+		proc.logger.Debug("not found", zap.Int("statusCode", resp.StatusCode), zap.String("url", url))
 		return errs.NewNotFound(url)
-	}
+	case resp.StatusCode == 429:
+		proc.logger.Info("probably rate limit", zap.String("ra", resp.Header.Get("Retry-After")), zap.String("url", url))
+		return nil
+	case resp.StatusCode >= 500 && resp.StatusCode <= 599:
+		proc.logger.Info("ignoring the url validation due to problems on the remote server", zap.Int("statusCode", resp.StatusCode), zap.String("url", url))
+		return nil
+	case 200 <= resp.StatusCode && resp.StatusCode <= 299:
+		// check just the first 10 KB of the body
+		bodyBytes, err := io.ReadAll(io.LimitReader(resp.Body, 10240))
+		if err != nil {
+			// we can't read body, something is off
+			return err
+		}
+		if len(bodyBytes) == 0 {
+			// body is empty, doesn't count as a healthy URL
+			return errs.NewEmptyBody(url)
+		}
 
-	// check just the first 4 KB of the body
-	bodyBytes, err := io.ReadAll(io.LimitReader(r.Body, 10240))
-	if err != nil {
-		// we can't read body, something is off
-		return err
-	}
-	if len(bodyBytes) == 0 {
-		// body is empty, doesn't count as a healthy URL
-		return errs.NewEmptyBody(url)
-	}
+		body := strings.ToLower(string(bodyBytes))
+		if strings.Contains(body, "404") ||
+			strings.Contains(body, "does not contain the path") ||
+			strings.Contains(body, "not found") {
 
-	body := strings.ToLower(string(bodyBytes))
-	if strings.Contains(body, "404") ||
-		strings.Contains(body, "does not contain the path") ||
-		strings.Contains(body, "not found") {
-
-		return errs.NewNotFound(url)
-	} else {
+			return errs.NewNotFound(url)
+		} else {
+			return nil
+		}
+	default:
+		proc.logger.Warn("unexpected status", zap.Int("statusCode", resp.StatusCode), zap.String("url", url))
 		return nil
 	}
 }
