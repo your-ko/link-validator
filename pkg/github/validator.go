be653cd543479d3c002b0d748414961cd5efc2bd
your-ko <>
2025-12-05 08:07:35 +0100

    Improve logging

--- pkg/github/validator.go
+++ pkg/github/validator.go
@@ -136,7 +136,7 @@ func (proc *LinkProcessor) Process(ctx context.Context, url string, _ string) er
 	if !ok {
 		return fmt.Errorf("unsupported GitHub request type %q. Report an issue", gh.typ)
 	}
-	slog.Debug("using", slog.String("handler", entry.name))
+	slog.Debug("using", slog.String("handler", fmt.Sprintf("github/%s", entry.name)))
 
 	return mapGHError(url, entry.fn(ctx, client, gh.owner, gh.repo, gh.ref, gh.path, gh.anchor))
 }
