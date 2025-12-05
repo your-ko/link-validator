c4d64522544821ccdc4c6dcf8b80873b0f3a78d5
your-ko <>
2025-12-05 06:19:45 +0100

    set the logLevel

--- cmd/link-validator/main.go
+++ cmd/link-validator/main.go
@@ -6,8 +6,6 @@ import (
 	"link-validator/pkg/config"
 	"log/slog"
 	"os"
-
-	"go.uber.org/zap"
 )
 
 var GitCommit string
@@ -15,31 +13,6 @@ var Version string
 var BuildDate string
 
 func main() {
-	//logger := link_validator.Init(link_validator.LogLevel())
-
-	sLogger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	slog.SetDefault(sLogger)
-
-	//defer func(logger *zap.Logger) {
-	//	_ = logger.Sync()
-	//}(logger)
-
-	// Panic guard to log stacktrace if app crashes
-	//defer func() {
-	//	if r := recover(); r != nil {
-	//		logger.Error("panic: application crashed",
-	//			zap.Any("panic", r),
-	//			zap.Stack("stack"),
-	//		)
-	//		os.Exit(1)
-	//	}
-	//}()
-
-	slog.Info("Starting Link Validator",
-		slog.String("version", Version),
-		slog.String("build date", BuildDate),
-		slog.String("git commit", GitCommit),
-	)
 
 	cfg := config.Default()
 	if cfgReader, err := os.Open(".link-validator.yaml"); err == nil {
@@ -48,52 +21,63 @@ func main() {
 			_ = cfgReader.Close()
 		}()
 	} else {
-		slog.Info("can't open config file .link-validator.yaml, skipping it")
+		slog.Warn("can't open config file .link-validator.yaml, skipping it")
 	}
 	cfg, err := cfg.Load()
 	if err != nil {
-		slog.Error("can't initialise, exiting", zap.Error(err))
+		slog.Error("can't initialise, exiting", slog.Any("error", err))
 		os.Exit(1)
 	}
+
+	sLogger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: cfg.LogLevel,
+	}))
+	slog.SetDefault(sLogger)
+
+	slog.Info("Starting Link Validator",
+		slog.Group("version",
+			slog.String("v", Version),
+			slog.String("build date", BuildDate),
+			slog.String("git commit", GitCommit),
+		))
 
 	if cfg.CorpGitHubUrl != "" && cfg.CorpPAT == "" {
 		slog.Warn("it seems you set CORP_URL but didn't provide CORP_PAT. Expect false negatives because the " +
 			"link won't be able to fetch corl github without pat")
 	}
 
-	slog.Debug("Running with parameters",
-		//slog.Group()
-		slog.String("LOG_LEVEL", os.Getenv("LOG_LEVEL")),
-		slog.Any("FILE_MASKS", cfg.FileMasks),
-		slog.Any("FILES", cfg.Files),
-		slog.Any("IGNORED_DOMAINS", cfg.IgnoredDomains),
-		//zap.String("LOOKUP_PATH", cfg.LookupPath), // not implemented yet
-		slog.Any("EXCLUDE", cfg.Exclude),
-		slog.String("CORP_URL", cfg.CorpGitHubUrl),
-		slog.Duration("TIMEOUT", cfg.Timeout),
-	)
-	os.Exit(1)
+	slog.Debug("Running with",
+		slog.Group("config",
+			slog.String("LOG_LEVEL", os.Getenv("LOG_LEVEL")),
+			slog.Any("FILE_MASKS", cfg.FileMasks),
+			slog.Any("FILES", cfg.Files),
+			slog.Any("IGNORED_DOMAINS", cfg.IgnoredDomains),
+			//slog.String("LOOKUP_PATH", cfg.LookupPath), // not implemented yet
+			slog.Any("EXCLUDE", cfg.Exclude),
+			slog.String("CORP_URL", cfg.CorpGitHubUrl),
+			slog.Duration("TIMEOUT", cfg.Timeout),
+		))
 
 	validator := link_validator.New(cfg)
 
 	filesList, err := validator.GetFiles()
 	if err != nil {
-		slog.Error("Error generating file list", zap.Error(err))
+		slog.Error("Error generating file list", slog.Any("err", err))
 		os.Exit(1)
 	}
-	slog.Debug("Found files", zap.Strings("files", filesList))
+	slog.Debug("Found files", slog.Any("files", filesList))
 
 	ctx := context.Background()
 	stats := validator.ProcessFiles(ctx, filesList)
 	if stats.Errors != 0 {
-		slog.Error("Errors found:", zap.Int("errors", stats.Errors))
+		slog.Error("Errors found:", slog.Int("errors", stats.Errors))
 	}
 	if stats.NotFoundLinks > 0 {
-		slog.Error("Links not found", zap.Int("links", stats.NotFoundLinks))
+		slog.Error("Links not found", slog.Int("links", stats.NotFoundLinks))
 	}
-	slog.Info("Files processed", zap.Int("files", stats.Files))
-	slog.Info("Links processed", zap.Int("links", stats.TotalLinks))
-	slog.Info("Lines processed", zap.Int("lines", stats.Lines))
+	slog.Info("Files processed", slog.Int("files", stats.Files))
+	slog.Info("Links processed", slog.Int("links", stats.TotalLinks))
+	slog.Info("Lines processed", slog.Int("lines", stats.Lines))
 
 	if stats.Errors > 0 || stats.NotFoundLinks > 0 {
 		os.Exit(1)
